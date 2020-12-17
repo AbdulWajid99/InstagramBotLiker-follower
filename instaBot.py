9d839a163bc14408a1fa513de44e34fe0ec3622b
Abdul Wajid <49652572+AbdulWajid99@users.noreply.github.com>
2020-12-18 02:08:14 +0500

    Instagram auto liker all done

--- instaBot.py
+++ instaBot.py
@@ -9,7 +9,7 @@ class InstaBot:
         self.username = username
         self.password = password
         self.driver = webdriver.Chrome(
-            executable_path="E:\Data_Scientist\Python\Projects\Intagram like bot\chromedriver_win32\chromedriver.exe")
+            executable_path="path")
 
 # to login from given username and password
     def login(self):
@@ -22,12 +22,39 @@ class InstaBot:
         password.send_keys(self.password)  # fill password with given password
         password.submit()  # submit it
         time.sleep(3)
+    #   password.submit()
+        time.sleep(3)
 
-        password.submit()
-        time.sleep()
+# search for id
+    def searchHashtags(self, hashtag):
+        driver = self.driver
+        driver.get("https://www.instagram.com/"+hashtag)
+
+# like the posts
+    def likePhotos(self, num):
+        driver = self.driver
+        driver.find_element_by_class_name("v1Nh3").click()
+
+        i = 1
+        while i <= num:
+            time.sleep(1)
+            driver.find_element_by_class_name(
+                "fr66n").click()  # like a posts  _8-yf5
+            driver.find_element_by_class_name(
+                "coreSpriteRightPaginationArrow").click()  # Go to next post
+            time.sleep(1)
+            i = i+1
+
+        driver.get("https://www.instagram.com/")  # Back to Insta main page
+        time.sleep(2)
 
 
 # first function give your insta id uname and pass
-instaBot = InstaBot("deadtillnow__", "ronaldocr7")
+instaBot = InstaBot("name", "password")
 # second function login with given credentials
 instaBot.login()
+# third function search your id
+instaBot.searchHashtags("name")
+# fourth one selects a picture to like
+num = 19  # number of pics to like
+instaBot.likePhotos(num)
