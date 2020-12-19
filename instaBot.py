e4fcdf932a2bafee87a6d097ec6fcea60bb66713
Abdul Wajid <49652572+AbdulWajid99@users.noreply.github.com>
2020-12-19 23:23:25 +0500

    Instagram auto liker updated

--- instaBot.py
+++ instaBot.py
@@ -1,21 +1,23 @@
+#import dependies
 import selenium
 from selenium import webdriver
 import time
 
 
-class InstaBot:
-    # constructore for variable
+class InstaBot_like:
+    # constructer for variable
     def __init__(self, username, password):
         self.username = username
         self.password = password
         self.driver = webdriver.Chrome(
-            executable_path="path")
+            executable_path="Chrome_Driver_path")
 
 # to login from given username and password
     def login(self):
         driver = self.driver
         driver.get("https://www.instagram.com/")  # open insta main page
         time.sleep(3)
+
         driver.find_element_by_name("username").send_keys(
             self.username)  # fill usermae with given username
         password = driver.find_element_by_name("password")
@@ -26,20 +28,22 @@ class InstaBot:
         time.sleep(3)
 
 # search for id
-    def searchHashtags(self, hashtag):
+    def findUser(self, userForlike):
         driver = self.driver
-        driver.get("https://www.instagram.com/"+hashtag)
+        driver.get("https://www.instagram.com/"+userForlike)
 
 # like the posts
-    def likePhotos(self, num):
+    def likePosts(self, num):
         driver = self.driver
         driver.find_element_by_class_name("v1Nh3").click()
 
         i = 1
         while i <= num:
             time.sleep(1)
+
             driver.find_element_by_class_name(
-                "fr66n").click()  # like a posts  _8-yf5
+                "fr66n").click()  # like a posts  #_8-yf5
+
             driver.find_element_by_class_name(
                 "coreSpriteRightPaginationArrow").click()  # Go to next post
             time.sleep(1)
@@ -50,11 +54,13 @@ class InstaBot:
 
 
 # first function give your insta id uname and pass
-instaBot = InstaBot("name", "password")
+instaBot = InstaBot_like("yourid", "pass")
+
 # second function login with given credentials
 instaBot.login()
+
 # third function search your id
-instaBot.searchHashtags("name")
+instaBot.findUser("User To Follow")
 # fourth one selects a picture to like
 num = 19  # number of pics to like
 instaBot.likePhotos(num)
